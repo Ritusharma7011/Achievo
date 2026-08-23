114b2e2403721dae55b12b361d644be8881f0ea0
Ritusharma7011 <ritukumari10904@gmail.com>
2026-08-23 19:23:26 +0530

    Fix Gmail SMTP configuration

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,33 +1,29 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const mailSender = async (email, title, body) =>{
-    try{
-        let transporter = nodemailer.createTransport({
-            host : process.env.MAIL_HOST,
-            port: 465,
-            secure: true,
-            auth :{
-                user : process.env.MAIL_USER,
-                pass : process.env.MAIL_PASS,
-            } 
-        })
+const mailSender = async (email, title, body) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
 
-        let info = await transporter.sendMail({
-            from : "Achievo",
-            to : `${email}`,
-            subject : `${title}`,
-            html : `${body}`
-        })
-        
-        console.log(info);
+        const info = await transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: email,
+            subject: title,
+            html: body,
+        });
+
+        console.log("Mail sent:", info.messageId);
         return info;
+    } catch (error) {
+        console.log("Mail error:", error);
+        throw error;
     }
-    catch(error){
-        console.log(error);
-    }
-
-}
+};
 
 module.exports = mailSender;
-
