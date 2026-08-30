2e927f4bb0b3314a2e5fd80c403310d13655c0d7
Ritusharma7011 <ritukumari10904@gmail.com>
2026-08-31 02:27:34 +0530

    Case error resolved

--- src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { MdRateReview } from "react-icons/md";
-import IconBtn from "../../Common/IconBtn"
+import IconBtn from "../../common/IconBtn"
 
 import {
   FaChevronLeft,
